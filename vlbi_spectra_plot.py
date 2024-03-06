e21d8ddc7a29ea4d1e67dfa6dbfa60aaa24631a2
sklandrausis <janis.akmenskalns@gmail.com>
2024-03-06 12:56:49 +0200

    add single file plotting script

--- vlbi_spectra_plot.py
+++ vlbi_spectra_plot.py
@@ -1,8 +1,25 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import sys
 
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+from parsers.configparser_ import ConfigParser
+
+
+def get_configs(section, key):
+    """
+
+    :param section: configuration file section
+    :param key: configuration file sections key
+    :return: configuration file section key value
+    """
+    config_file_path = "config/config.cfg"
+    config = ConfigParser(config_file_path)
+    return config.get_config(section, key)
 
 def read_spectrum_file(spectrum_file):
     vel_rr = []
@@ -18,23 +35,26 @@ def read_spectrum_file(spectrum_file):
                 continue
             else:
                 line = lines[index]
-                data_tmp = line.split()
-                data_tmp = [d.strip() for d in data_tmp]
+                if "RR" in line or "LL" in line:
+                    data_tmp = line.split()
+                    data_tmp = [d.strip() for d in data_tmp]
 
-                pol = data_tmp[2]
-                if pol == "RR":
-                    vel_rr.append(data_tmp[4])
-                    amp_rr.append(data_tmp[5])
-                elif pol == "LL":
-                    vel_ll.append(data_tmp[4])
-                    amp_ll.append(data_tmp[5])
+                    pol = data_tmp[2]
+                    if pol == "RR":
+                        vel_rr.append(data_tmp[4])
+                        amp_rr.append(data_tmp[5])
+                    elif pol == "LL":
+                        vel_ll.append(data_tmp[4])
+                        amp_ll.append(data_tmp[5])
 
     return (np.array(vel_rr, dtype=float), np.array(vel_ll, dtype=float),
             np.array(amp_rr, dtype=float), np.array(amp_ll, dtype=float))
 
 
-def main():
-    file = "W3OH_LINE.TXT"
+def main(source_name, obs_name):
+    monitoring_path = get_configs("paths", "monitoring_path") + "/"
+    file = monitoring_path + source_name + "_LINE" + "_" + obs_name + ".TXT"
+
     spectrum = read_spectrum_file(file)
     plt.plot(spectrum[0], spectrum[2], label="RCP")
     plt.plot(spectrum[1], spectrum[3], label="LRP")
@@ -42,9 +62,17 @@ def main():
     plt.xlabel("Velocity [km/s]", labelpad=14)
     plt.ylabel("Flux [Jy]", labelpad=10)
     plt.legend()
+    plt.tight_layout()
     plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='''Plot a single file. ''', epilog="""plot monitoring.""")
+    parser.add_argument("source", help="Experiment source", type=str, default="")
+    parser.add_argument("exper", help="Experiment name", type=str)
+    parser.add_argument("-c", "--config", help="Configuration cfg file", type=str,
+                        default="config/config.cfg")
+    parser.add_argument("-v", "--version", action="version", version='%(prog)s - Version 1.0')
+    args = parser.parse_args()
+    main(args.source, args.exper)
     sys.exit()
